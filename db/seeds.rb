54829e8314d6d88c3c63a7170273d85b8d24acde
Victor Marxen <victor.marxen@hotmail.de>
2022-08-16 17:26:23 +0100

    Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new
+  article.title = Faker::Lorem.sentence
+  article.content = Faker::Lorem.paragraph
+  article.save
+end
+puts 'Finished!'
